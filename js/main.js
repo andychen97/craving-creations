891df2151f670d6b73b6b59dddc96d953b2368f4
Andy Chen <chen.andy18@gmail.com>
2022-06-14 19:20:36 +0000

    revised code to match github comments.

--- js/main.js
+++ js/main.js
@@ -29,12 +29,10 @@ function apiSearch(customerInput) {
   var originalUrl = 'https://tasty.p.rapidapi.com/recipes/list?from=0&size=8&q=';
   originalUrl += customerInput;
 
-  xhr.addEventListener('readystatechange', function () {
-    if (this.readyState === this.DONE) {
-      var jsonResponse = JSON.parse(this.responseText);
-      var results = jsonResponse.results;
-      parseResponse(results);
-    }
+  xhr.addEventListener('load', function () {
+    var jsonResponse = JSON.parse(this.responseText);
+    var results = jsonResponse.results;
+    parseResponse(results);
   });
 
   xhr.open('GET', originalUrl);
@@ -47,53 +45,21 @@ function apiSearch(customerInput) {
 function parseResponse(apiResponse) {
   for (let i = 0; i < apiResponse.length; i++) {
     if (!apiResponse[i].recipes) {
-      // var recipe = apiResponse[i];
-      // var name = recipe.name;
-      // var description = recipe.description;
-      // var recipeImage = recipe.thumbnail_url;
-      // var recipeIngredients = recipe.sections[0].components;
-      // var recipeInstructions = recipe.instructions;
-      // console.log('recipe: ', recipe);
-      // console.log('name: ', name);
-      // console.log('description: ', description);
-      // console.log('recipeImage: ', recipeImage);
-      // console.log('recipeIngredients: ', recipeIngredients);
-      // console.log('recipeInstructions: ', recipeInstructions);
-      // console.log('\n');
+      var recipe = apiResponse[i];
+      var name = recipe.name;
+      var description = recipe.description;
+      var recipeImage = recipe.thumbnail_url;
+      var recipeIngredients = recipe.sections[0].components;
+      var recipeInstructions = recipe.instructions;
+      console.log('recipe: ', recipe);
+      console.log('name: ', name);
+      console.log('description: ', description);
+      console.log('recipeImage: ', recipeImage);
+      console.log('recipeIngredients: ', recipeIngredients);
+      console.log('recipeInstructions: ', recipeInstructions);
+      console.log('\n');
     } else {
       parseResponse(apiResponse[i].recipes);
     }
   }
 }
-
-// var data = null;
-
-// var xhr = new XMLHttpRequest();
-// xhr.withCredentials = true;
-
-// xhr.addEventListener('readystatechange', function () {
-//   if (this.readyState === this.DONE) {
-//     var jsonResponse = JSON.parse(this.responseText);
-//     var results = jsonResponse.results[8].item.recipes;
-//     // var topTenRecipes = results.recipes.slice(0, 10);
-//     // console.log('topTenRecipes', results);
-//     for (let i = 0; i < results.length; i++) {
-//       var recipe = results[i];
-//       var name = recipe.name;
-//       var recipeImage = recipe.thumbnail_url;
-//       var recipeIngredients = recipe.sections[0].components;
-//       var recipeInstructions = recipe.instructions;
-//       // console.log('example: receipe[0]: ', recipe);
-//       // console.log('name: ', name);
-//       // console.log('recipeImage: ', recipeImage);
-//       // console.log('recipeIngredients: ', recipeIngredients);
-//       // console.log('recipeInstructions: ', recipeInstructions);
-//     }
-//   }
-// });
-
-// xhr.open('GET', 'https://tasty.p.rapidapi.com/feeds/list?size=5&timezone=%2B0700&vegetarian=false&from=0');
-// xhr.setRequestHeader('X-RapidAPI-Key', '0cee8fcae5mshc138c2d6a69e94cp1c0984jsn59f501227ea8');
-// xhr.setRequestHeader('X-RapidAPI-Host', 'tasty.p.rapidapi.com');
-// xhr.withCredentials = false;
-// xhr.send(data);
