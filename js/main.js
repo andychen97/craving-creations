e375c69d7c84e3a9a33ef8c688b796399c7e4c84
Andy Chen <chen.andy18@gmail.com>
2022-06-15 06:15:15 +0000

    completed feature 2 of ajax project.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
+// /* global data */
 var $navSearch = document.querySelector('form[name="navSearch"]');
 var $search = document.querySelector('form[name="heroSearch"]');
 var $navCustomerInput = document.querySelector('input[name="navInput"]');
 var $regularCustomerInput = document.querySelector('input[name="heroInput"]');
+var $ul = document.querySelector('ul');
+var $h4Results = document.querySelector('h4[id="results"]');
 
 $navSearch.addEventListener('submit', searchCallback);
 $search.addEventListener('submit', searchCallback);
 
 function searchCallback(event) {
   event.preventDefault();
+  resetSearch();
   var customerInput;
   if ($navCustomerInput.value) {
     customerInput = $navCustomerInput.value;
@@ -18,6 +22,7 @@ function searchCallback(event) {
   apiSearch(customerInput);
   $navSearch.reset();
   $search.reset();
+  $h4Results.className = '';
 }
 
 function apiSearch(customerInput) {
@@ -51,15 +56,52 @@ function parseResponse(apiResponse) {
       var recipeImage = recipe.thumbnail_url;
       var recipeIngredients = recipe.sections[0].components;
       var recipeInstructions = recipe.instructions;
-      console.log('recipe: ', recipe);
-      console.log('name: ', name);
-      console.log('description: ', description);
-      console.log('recipeImage: ', recipeImage);
-      console.log('recipeIngredients: ', recipeIngredients);
-      console.log('recipeInstructions: ', recipeInstructions);
-      console.log('\n');
+      var singleRecipe = {
+        name,
+        description,
+        image: recipeImage,
+        recipeIngredients,
+        recipeInstructions,
+        tempEntryId: data.tempNextEntryId++
+      };
+      data.tempEntries.unshift(singleRecipe);
+      $ul.prepend(createCards(data.tempEntries[0]));
     } else {
       parseResponse(apiResponse[i].recipes);
     }
+  }
+}
+
+var $resultcount = document.querySelector('span[id="result-count"]');
+var matchCount = 0;
+
+function createCards(entries) {
+  var $li = document.createElement('li');
+  var $divCol = document.createElement('div');
+  $divCol.className = 'col-4';
+  $li.appendChild($divCol);
+  var $divCard = document.createElement('div');
+  $divCard.className = 'picture-card';
+  $divCol.appendChild($divCard);
+  var $image = document.createElement('img');
+  $image.setAttribute('src', entries.image);
+  $image.className = 'picture';
+  $divCard.appendChild($image);
+  var $h3Title = document.createElement('h3');
+  $h3Title.className = 'card-title-text';
+  $h3Title.textContent = entries.name;
+  $divCard.appendChild($h3Title);
+  matchCount++;
+  $resultcount.textContent = matchCount;
+
+  return $li;
+}
+
+function resetSearch() {
+  matchCount = 0;
+  data.tempEntries = [];
+  var $createdLi = document.querySelectorAll('li');
+  for (let i = 0; i < $createdLi.length; i++) {
+    $ul.removeChild($createdLi[i]);
   }
 }
