3a9b0caba205d72fd58a03a925708e8447b5895e
Andy Chen <chen.andy18@gmail.com>
2022-06-16 22:47:36 +0000

    dom tree completed to generate a clicked recipe.

--- js/main.js
+++ js/main.js
@@ -106,6 +106,7 @@ function createCards(entries) {
   matchCount++;
   $resultcount.textContent = matchCount;
   $li.addEventListener('click', singleRecipeInstruction);
+  $li.entries = entries;
   return $li;
 }
 
@@ -121,20 +122,118 @@ function resetSearch() {
 function singleRecipeInstruction(event) {
   data.view = 'recipe-page';
   viewSwap();
+  var clickedEntry = event.currentTarget.entries;
   var $div = document.createElement('div');
   $div.className = 'row';
   var $divTitle = document.createElement('div');
   $divTitle.className = 'col-1 margin-20px';
   $div.appendChild($divTitle);
-  // var $h2Title = document.createElement('h2');
-  // $h2Title.textContent = data.tempEntries
+  var $h2Title = document.createElement('h2');
+  $h2Title.textContent = clickedEntry.name;
+  $divTitle.appendChild($h2Title);
+  var $pDescript = document.createElement('p');
+  $pDescript.textContent = clickedEntry.description;
+  $divTitle.appendChild($pDescript);
+  $singeRecipePage.appendChild($div);
+  var $divServing = document.createElement('div');
+  $divServing.className = 'row align-center';
+
+  var $h4Calories = document.createElement('h4');
+  $h4Calories.className = 'serving-facts';
+  if (clickedEntry.calories === undefined) {
+    $h4Calories.textContent = 'Calories' + '\r\n' + '-';
+  } else {
+    $h4Calories.textContent = 'Calories' + '\r\n' + clickedEntry.calories;
+  }
+  $divServing.appendChild($h4Calories);
+  var $h4Carbs = document.createElement('h4');
+  $h4Carbs.className = 'serving-facts';
+  if (clickedEntry.carbohydrates === undefined) {
+    $h4Carbs.textContent = 'Carbs' + '\r\n' + '-';
+  } else {
+    $h4Carbs.textContent = 'Carbs' + '\r\n' + clickedEntry.carbohydrates;
+  }
+  $divServing.appendChild($h4Carbs);
+  var $h4Fats = document.createElement('h4');
+  $h4Fats.className = 'serving-facts';
+  if (clickedEntry.fats === undefined) {
+    $h4Fats.textContent = 'Fats' + '\r\n' + '-';
+  } else {
+    $h4Fats.textContent = 'Fats' + '\r\n' + clickedEntry.fats;
+  }
+  $divServing.appendChild($h4Fats);
+  var $h4Protein = document.createElement('h4');
+  $h4Protein.className = 'serving-facts';
+  if (clickedEntry.protein === undefined) {
+    $h4Protein.textContent = 'Protein' + '\r\n' + '-';
+  } else {
+    $h4Protein.textContent = 'Protein' + '\r\n' + clickedEntry.protein;
+  }
+  $divServing.appendChild($h4Protein);
+  var $h4Fiber = document.createElement('h4');
+  $h4Fiber.className = 'serving-facts';
+  if (clickedEntry.fiber === undefined) {
+    $h4Fiber.textContent = 'Fiber' + '\r\n' + '-';
+  } else {
+    $h4Fiber.textContent = 'Fiber' + '\r\n' + clickedEntry.fiber;
+  }
+  $divServing.appendChild($h4Fiber);
+  var $h4Sugar = document.createElement('h4');
+  $h4Sugar.className = 'serving-facts-last';
+  if (clickedEntry.sugar === undefined) {
+    $h4Sugar.textContent = 'Sugar' + '\r\n' + '-';
+  } else {
+    $h4Sugar.textContent = 'Sugar' + '\r\n' + clickedEntry.sugar;
+  }
+  $divServing.appendChild($h4Sugar);
+  $singeRecipePage.appendChild($divServing);
+  var $divImageIngredient = document.createElement('div');
+  $divImageIngredient.className = 'row';
+  var $divImageCol = document.createElement('div');
+  $divImageCol.className = 'col-3-5';
+  $divImageIngredient.appendChild($divImageCol);
+  var $recipeImage = document.createElement('img');
+  $recipeImage.className = 'image-ingredient-page';
+  $recipeImage.setAttribute('src', clickedEntry.image);
+  $divImageCol.appendChild($recipeImage);
+  var $divIngredientCol = document.createElement('div');
+  $divIngredientCol.className = 'col-2-5';
+  $divImageIngredient.appendChild($divIngredientCol);
+  var $h4IngredientTitle = document.createElement('h4');
+  $h4IngredientTitle.textContent = 'INGREDIENTS';
+  $h4IngredientTitle.className = 'ingredient-title';
+  $divIngredientCol.appendChild($h4IngredientTitle);
+  var $ulIngredients = document.createElement('ul');
+  $ulIngredients.className = 'ingredient-list';
+  $divIngredientCol.append($ulIngredients);
+  for (var i = 0; i < clickedEntry.recipeIngredients.length; i++) {
+    var $liIngredients = document.createElement('li');
+    $liIngredients.textContent = $liIngredients.textContent + clickedEntry.recipeIngredients[i].raw_text;
+    $ulIngredients.appendChild($liIngredients);
+  }
+  $singeRecipePage.appendChild($divImageIngredient);
+  var $divInstructionCol = document.createElement('div');
+  $divInstructionCol.className = 'col-1';
+  var $h4InstructionTitle = document.createElement('h4');
+  $h4InstructionTitle.className = 'instructions-title';
+  $h4InstructionTitle.textContent = 'INSTRUCTIONS';
+  $divInstructionCol.appendChild($h4InstructionTitle);
+  var $olInstructions = document.createElement('ol');
+  $olInstructions.className = 'instructions-list';
+  $divInstructionCol.appendChild($olInstructions);
+  for (var j = 0; j < clickedEntry.recipeInstructions.length; j++) {
+    var $liInstructions = document.createElement('li');
+    $liInstructions.textContent = $liInstructions.textContent + clickedEntry.recipeInstructions[j].display_text;
+    $olInstructions.appendChild($liInstructions);
+  }
+  $singeRecipePage.appendChild($divInstructionCol);
 }
 
 function viewSwap() {
   if (data.view === 'home-page') {
     $homePage.classList.remove('hidden');
     $singeRecipePage.classList.add('hidden');
-  } else if (data.view === 'single-recipe') {
+  } else if (data.view === 'recipe-page') {
     $homePage.classList.add('hidden');
     $singeRecipePage.classList.remove('hidden');
   }
